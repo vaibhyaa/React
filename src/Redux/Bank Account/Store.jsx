3527d8f19259ef48b68ab874c6113b6a015353d3
vaibhyaa <vaibhavshinde1318@gmail.com>
2025-12-26 12:46:53 +0530

    redux store/basic/combineReducers

--- src/Redux/Bank Account/Store.jsx
+++ src/Redux/Bank Account/Store.jsx
@@ -1,11 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { useReducer } from "react";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import reducerFunAccount from "./Features/Account/AccountOperations";
 import reducerFunCustomer from "./Features/Customer/CustomerOperations";
+import { thunk } from "redux-thunk";
 
-export const store = configureStore({
-  reducer: {
-    Account: reducerFunAccount,
-    Customer: reducerFunCustomer,
-  },
+const rootReducer = combineReducers({
+  // They are simply keys (property names) (Account and Customer ) of the global Redux state object
+  Account: reducerFunAccount,
+  Customer: reducerFunCustomer,
 });
-export default store
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// const [state, dispatch] = useReducer(reducer, initialState);
+// const [state, dispatch] = useReducer(reducer, {
+//   balance: 0,
+//   loan: 0,
+//   loanPurpose: "",
+// });
